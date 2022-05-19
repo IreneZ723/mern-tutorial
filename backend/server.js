f255b0f4e37adabf80d597f227f9a724f524732a
irenez0723 <zhang.yanli@northeastern.edu>
2022-05-18 20:07:53 -0700

    add goal

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 // express -- backend web framework
 const express = require('express')
 const colors = require('colors')
@@ -5,6 +6,8 @@ const connectDB = require('./config/db')
 // environment variable 
 const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
+const req = require('express/lib/request')
+const { sendFile } = require('express/lib/response')
 const port = process.env.PORT || 5000
 connectDB()
 const app = express()
@@ -14,5 +17,13 @@ app.use(express.urlencoded({extended: false}))
 app.use('/api/goals',require('./routes/goalRoutes'))
 app.use('/api/users',require('./routes/userRoutes'))
 app.use(errorHandler)
+// serve frontend 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'../frontend/build')))
 
+    app.get('*',(req,res)=> res.sendFile(path.resolve(__dirname, '../','frontend','build','index.html')))
+}
+else{
+     app.get('/',(req,res) => res.send('Please set to production'))
+}
 app.listen(port,()=>console.log(`Server started on port ${port}`))
